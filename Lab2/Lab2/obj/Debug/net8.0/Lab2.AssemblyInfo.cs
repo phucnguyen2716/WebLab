1b843543aa90c378f646cdb419f48947525d3105
phucnguyen2716 <phucnguyen2716@gmail.com>
2025-12-01 17:28:42 +0700

    init lab2

--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1f7870728dc6ab1d6e7f6dd4a0bdbce2b6dc4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e282c434ec947cafd14a9f0f55c0844cd829d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
